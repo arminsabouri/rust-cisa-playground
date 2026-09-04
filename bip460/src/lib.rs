cb133458bd11e474e7b574946e6de443c1935e48
Armin Sabouri <me@arminsabouri.com>
2026-09-04 13:33:40 -0400

    Add AggMode for the two BIP 460 aggregation markers

--- bip460/src/lib.rs
+++ bip460/src/lib.rs
@@ -7,3 +7,26 @@ pub use bip459::{Message, PublicKey, verify as verify_full_agg};
 pub const HALF_AGG_MARKER: u8 = 0xbc;
 pub const FULL_AGG_MARKER: u8 = 0xbd;
 pub const SIGHASH_EPOCH_AGGREGATED: u8 = 0x01;
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum AggMode {
+    Half,
+    Full,
+}
+
+impl AggMode {
+    pub const fn as_byte(self) -> u8 {
+        match self {
+            AggMode::Half => HALF_AGG_MARKER,
+            AggMode::Full => FULL_AGG_MARKER,
+        }
+    }
+
+    pub const fn from_byte(byte: u8) -> Option<AggMode> {
+        match byte {
+            HALF_AGG_MARKER => Some(AggMode::Half),
+            FULL_AGG_MARKER => Some(AggMode::Full),
+            _ => None,
+        }
+    }
+}
